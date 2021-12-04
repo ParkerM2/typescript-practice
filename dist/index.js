1557cdb09a1050d279c1eff533de9e3a789e6258
Parker Manning <parkerlmanning@hotmail.com>
2021-12-04 14:56:36 -0600

    practicing typescript via traversy media typescrip crash course 2021

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let id = 5;
+// type inference - infers that its a number even if its not set
+console.log('id', id);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let id: number = 5;
+
+// type inference - infers that its a number even if its not set
+
+console.log('id', id)
