f86dbd2e5f58ddd8eaa18bad33863a929075ce71
Parker Manning <parkerlmanning@hotmail.com>
2021-12-06 10:16:14 -0600

    functions more practice

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,43 @@
 "use strict";
+// basic types
 let id = 5;
-// type inference - infers that its a number even if its not set
-console.log('id', id);
+let company = 'Company';
+let isPublished = true;
+let x = 'hello world';
+let age; // if not a number will through error
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'hello world'];
+// tuple ? = a finite ordered list (sequence) of elements
+// a n-tuple is a sequence (or ordered list) of n elements, where n is a non-negative integer
+let person = [1, 'Brad', false];
+// tuple array
+let employee;
+employee = [
+    [1, 'Parker'],
+    [1, 'Parker'],
+    [1, 'Parker'],
+    [1, 'Parker'],
+];
+// union (string or number)
+let pid = 22;
+// Enum set of named constants
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "up";
+    Direction1["Down"] = "down";
+    Direction1["Left"] = "left";
+    Direction1["Right"] = "right";
+})(Direction1 || (Direction1 = {}));
+// objects
+const user = {
+    id: 1,
+    name: 'John'
+};
+let cid = 1;
+let customerId = cid;
+// functions
+// parameters must be a type, as well as the returned value
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(4, 1));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,56 @@
+// basic types
 let id: number = 5;
+let company: string = 'Company';
+let isPublished: boolean = true;
+let x: any = 'hello world';
+let age: number; // if not a number will through error
 
-// type inference - infers that its a number even if its not set
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, 'hello world'];
 
-console.log('id', id)
+// tuple ? = a finite ordered list (sequence) of elements
+// a n-tuple is a sequence (or ordered list) of n elements, where n is a non-negative integer
+let person: [number, string, boolean] = [1, 'Brad', false]
+
+// tuple array
+let employee: [number, string][];
+employee = [
+    [1, 'Parker'],
+    [1, 'Parker'],
+    [1, 'Parker'],
+    [1, 'Parker'],
+];
+
+// union (string or number)
+let pid: string | number = 22;
+
+// Enum set of named constants
+enum Direction1 {
+    Up = 'up',
+    Down = 'down',
+    Left = 'left',
+    Right = 'right',
+}
+
+// objects
+const user:
+    {
+    id: number,
+    name: string
+    } = {
+    id: 1,
+    name: 'John'
+};
+
+let cid: any = 1;
+
+let customerId = cid as number;
+
+// functions
+
+// parameters must be a type, as well as the returned value
+function addNum(x: number, y: number): number {
+    return x + y
+}
+
+console.log(addNum(4,1))
